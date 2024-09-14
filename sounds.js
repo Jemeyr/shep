c7d9d52b4fa1a077d31e0de6f6c6af795e8369d2
Jemeyr <1980987+Jemeyr@users.noreply.github.com>
2024-09-13 22:33:21 -0700

    Working major scale sorta

--- sounds.js
+++ sounds.js
@@ -1,21 +1,23 @@
 
 
-
-var lowestFreq = 128;//50; //Lowest outputted frequency (seperate from what is generated)
-var highestFreq = 8192;//10000; //Highest outputted frequency (seperate from what is generated)
+//TODO ok so this is 6 powers of 2 apart, as in 7040/110 = 64 = 2^6
+//TODO therefore 1.0 in "phase" is functionally "6 octaves"
+var lowestFreq = 110;//A2
+var highestFreq = 7040;//A8
+var octaveSpan = 6;
 
 //calculated shit
 //Ok basically we need to find out what frequency is logarithmically 1 Nth (For n oscillators) of the range from low to high
 var logRange = Math.log(highestFreq) / Math.log(lowestFreq)
 
 
-
+//TODO check these are not borked and reenable
 function getFadeInCutoff(oscAmt){
-  return lowestFreq + Math.pow(lowestFreq, logRange/oscAmt)
+  return highestFreq;//lowestFreq + Math.pow(lowestFreq, logRange/oscAmt)
 }
 
 function getFadeOutCutoff(oscAmt){
-  return lowestFreq + Math.pow(lowestFreq, (logRange * (oscAmt - 1))/oscAmt)
+  return lowestFreq// + Math.pow(lowestFreq, (logRange * (oscAmt - 1))/oscAmt)
 }
 
 
@@ -24,7 +26,7 @@ function getFadeOutCutoff(oscAmt){
 //TODO TODO this uses a bunch of jankily far away state. Also the math seems wrong
 //TODO this maps a "phase" to a frequency. Phases should basically map a 0-1 range to some number of octaves (2-4?) with a shepard tone wrapping such that 0 = 1 
 function toFrequency(phase){
-  return  lowestFreq + Math.pow(lowestFreq, logRange * (phase == 0 ? 0.0000001 : phase))
+  return  lowestFreq * Math.pow(2, octaveSpan * phase);
 }
 
 //Phase here is a number between 0-1, I guess it should map to some number of oscillators
@@ -40,7 +42,7 @@ function createNote(phase, oscAmt, context){
 
   for (i = 0; i < oscAmt; i++) {
     osc = context.createOscillator();
-    osc.type = 'triangle'
+    osc.type = 'sawtooth'
     
     gain = context.createGain()
     gain.gain.value = 1
@@ -66,9 +68,10 @@ function createNote(phase, oscAmt, context){
     var fadeOutCutoff = getFadeOutCutoff(oscAmt); 
 
     for (i = 0; i< oscAmt; i++){
-      //split the phase spectrum by oscillator count, then add phase, which is really the offset and mod 1 to loop
-      var frequency = toFrequency(((i/oscAmt) + newPhase) % 1)
+      var frequency = toFrequency((newPhase + ((i === 0 ? 0 : Math.log(i+1)) / Math.log(2))/octaveSpan) %1.0 );
+      console.log(`Setting frequency(${i}) to ${frequency} [which is ${frequency / toFrequency((newPhase) % 1)}]`)
       this.oscs[i].frequency.value = frequency; //Apply new frequency to oscillator.
+
 
 
       //Kinda janky. Keep in mind each note has its own master, which scales overall note volume
@@ -85,7 +88,6 @@ function createNote(phase, oscAmt, context){
       } else if(frequency >= highestFreq) {
         this.gains[i].gain.value = 0;
       }
-
     }
   }
 
@@ -114,11 +116,9 @@ jQuery("document").ready(function($) {
     context = new AudioContext();
 
     //Config shit
-    var oscAmt = 12; //Amount of oscillators (10 by default)
-   
     var gainVal = 0.2; //Sets gain value. Keep low!
 
-    var intervalTime = 25;//how often to update things
+    var intervalTime = 500;//how often to update things
 
 
 
@@ -139,14 +139,9 @@ jQuery("document").ready(function($) {
 
     //Add some notes
 
-    var aNote = createNote(0.01, 6, context)
+    var aNote = createNote(0, 16, context)
     aNote.volume.connect(filter)
 
-    var bNote = createNote(0.02, 6, context)
-    bNote.volume.connect(filter)
-
-    var cNote = createNote(aNote.phase + 0.4, 6, context)
-    cNote.volume.connect(filter)
 
 
     filter.connect(compressor);
@@ -154,16 +149,21 @@ jQuery("document").ready(function($) {
     master.connect(analyser);
     analyser.connect(context.destination); //Connect compressor node to destination (usually speakers)
 
+
+    //Divide by the frequency range in powers of to(octaves) scalar and also 12, for chromatic
+    var denominator = octaveSpan*12;
+    var scaleIndex = 0;
+    var scale = [2.0,2.0,1.0,2.0,2.0,2.0,1.0]
+
     setInterval(function() {
       if ($('.btn-mute').hasClass('disable')) {
         master.gain.value = 0;
       } else {
         master.gain.value = gainVal;
       }
-      bNote.setPhase(bNote.phase + 0.0005)
-      aNote.setPhase(aNote.phase + 0.999)
-      cNote.setPhase(cNote.phase + 0.99999)
-
+      console.log(`scale index ${scaleIndex}`)
+      aNote.setPhase((aNote.phase + (scale[scaleIndex]/denominator))%1.0)
+      scaleIndex = (scaleIndex === (scale.length - 1)) ? 0 : scaleIndex + 1;
     }, intervalTime);
 
     /*Analyser*/
